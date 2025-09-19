008ff2ee32071ccfc4f74514458cc0b871647681
Mathias Bardram Johnbeck <Mathiasjohnbeck@gmail.com>
2025-09-19 14:40:04 +0200

    DTO's for printers and jobs

---
+++ printing/objects.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Job:
+    id: int
+    printer: str
+    user: str
+    status: str
+    pages: Optional[int] = None
+
+@dataclass
+class Printer:
+    name: str
+    status: str
+    enabled: bool = True
+    jobs: int = 0
+    since: Optional[str] = None
+    location: Optional[str] = None
+    description: Optional[str] = None
+    error: Optional[str] = None
+
+
